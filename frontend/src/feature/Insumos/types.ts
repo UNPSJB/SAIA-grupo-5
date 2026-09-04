9cfac83dc30c23a7352b4d2793e9cf736fcffbd9
villanuevaalex <alex_villanuevasoto@hotmail.com>
2026-09-04 17:59:47 -0300

    se agrego tupo enum a unidad_medida de Insumos

--- frontend/src/feature/Insumos/types.ts
+++ frontend/src/feature/Insumos/types.ts
@@ -1,5 +1,19 @@
+export const UnidadMedida = {
+    TONELADA: "t",
+    KILOGRAMO: "kg",
+    GRAMO: "g",
+    MILIGRAMO: "mg",
+    LITRO: "l",
+    DECILITRO: "dl",
+    MILILITRO: "ml",
+    UNIDAD: "ud",
+    DOCENA: "doc",
+} as const;
+
+export type UnidadMedida = typeof UnidadMedida[keyof typeof UnidadMedida];
+
 export type Insumo = {
     id: number
     nombre: string
-    unidad_medida: string
+    unidad_medida: UnidadMedida
 }
