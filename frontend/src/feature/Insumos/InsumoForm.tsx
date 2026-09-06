dcc03afb5369a94a47372afe2d6f646ab1f2ead8
tobisabbione <tobiiseba714@gmail.com>
2026-09-06 14:29:00 -0300

    EditarInsumoPage terminado y correcciones en InsumoForm

--- frontend/src/feature/Insumos/InsumoForm.tsx
+++ frontend/src/feature/Insumos/InsumoForm.tsx
@@ -4,17 +4,19 @@ import { UnidadMedida } from './types';
 
 interface InsumoFormProps{
   textoBoton: string;
-  onSubmit: (datos: {nombre: string; unidad_medida: string}) => void;
+  onSubmit: (datos: {nombre: string; unidad_medida: UnidadMedida}) => void;
+  valoresIniciales? : {nombre: string; unidad_medida: UnidadMedida}
 }
 
-export function InsumoForm({ textoBoton, onSubmit }: InsumoFormProps) {
-  const [nombre, setNombre] = useState("");
-  const [unidadMedida, setUnidadMedida] = useState("");
+export function InsumoForm({ textoBoton, onSubmit, valoresIniciales}: InsumoFormProps) {
+  // Usamos useState y definimos que los valores pueden ser vacios por si se crea un nuevo insumo o que tengan un valor anterior para mostrarlos en caso de editar el insumo
+  const [nombre, setNombre] = useState(valoresIniciales?.nombre || "");
+  const [unidadMedida, setUnidadMedida] = useState<UnidadMedida | "">(valoresIniciales?.unidad_medida || "");     // Se agrega el <UnidadMedida | ""> para exigir que los valores unicamente puedan ser los de las unidades de medidas que definio Alex
 
   // El handleSubmit se usa para que no actualice la pagina al apretar el boton y envia a la pagina que lo utilice los datos
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {   // El FormEvent esta deprecated, consultar al profe cual se usa actualmente
     e.preventDefault();
-    onSubmit({ nombre, unidad_medida: unidadMedida});
+    onSubmit({ nombre, unidad_medida: unidadMedida as UnidadMedida});
   }
 
   return (
@@ -26,7 +28,7 @@ export function InsumoForm({ textoBoton, onSubmit }: InsumoFormProps) {
 
       <Form.Group className="mb-3" controlId="formUnidadMedida">
         <Form.Label>Unidad de medida</Form.Label>
-        <Form.Select value={unidadMedida} onChange={(e) => setUnidadMedida(e.target.value)} >
+        <Form.Select value={unidadMedida} onChange={(e) => setUnidadMedida(e.target.value as UnidadMedida)} >
           <option value="" disabled> Seleccione una unidad de medida</option>
           {Object.entries(UnidadMedida).map(([clave, valor]) => (     // Esto transforma el enum de types en una lista de clave valor para mostrarlo en las opciones
             <option key={clave} value={valor}>
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import App from './App.tsx'
 import { createBrowserRouter } from 'react-router-dom';
 import { InsumosPage } from './feature/Insumos/InsumosPage.tsx';
 import { NuevoInsumoPage } from './feature/Insumos/NuevoInsumoPage.tsx';
+import { EditarInsumoPage } from './feature/Insumos/EditarInsumoPage.tsx';
 import { HomePage } from './feature/Home/HomePage.tsx';
 import { EquiposPage } from './feature/Equipos/EquiposPage.tsx';
 import { PersonalPage } from './feature/Personal/PersonalPage.tsx';
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: "insumos", element: <InsumosPage /> },
       { path: "insumos/new", element: <NuevoInsumoPage />},
+      { path: "insumos/:id/edit", element: <EditarInsumoPage/>},
       { path: "equipos", element: <EquiposPage /> },
       { path: "personal", element: <PersonalPage /> },
     ]
