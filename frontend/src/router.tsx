748bafabccf2146e9e4ff8a366f8b539c9aabcc5
Belen <llampabelenok1@gmail.com>
2026-09-07 16:04:39 -0300

    Crear nuevo equipo

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -5,6 +5,7 @@ import { NuevoInsumoPage } from './feature/Insumos/NuevoInsumoPage.tsx';
 import { EditarInsumoPage } from './feature/Insumos/EditarInsumoPage.tsx';
 import { HomePage } from './feature/Home/HomePage.tsx';
 import { EquiposPage } from './feature/Equipos/EquiposPage.tsx';
+import { NuevoEquipoPage } from './feature/Equipos/NuevoEquipoPage.tsx';
 import { PersonalPage } from './feature/Personal/PersonalPage.tsx';
 
 const router = createBrowserRouter([
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
       { path: "insumos/new", element: <NuevoInsumoPage />},
       { path: "insumos/:id/edit", element: <EditarInsumoPage/>},
       { path: "equipos", element: <EquiposPage /> },
+      { path: "equipos/new", element: <NuevoEquipoPage />},
       { path: "personal", element: <PersonalPage /> },
     ]
   },
