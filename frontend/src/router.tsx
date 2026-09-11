2ca8aa1ec42bde22c32f6fa36cfe522a50f4099f
riquelme.juan261 <riquelme.juan261@gmail.com>
2026-09-11 15:06:50 -0300

    Rama lista para el merge

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,13 +1,9 @@
 import App from './App.tsx'
 import { createBrowserRouter } from 'react-router-dom';
-import { NuevoInsumoPage } from './feature/Insumos/pages/NuevoInsumoPage.tsx';
-import { EditarInsumoPage } from './feature/Insumos/pages/EditarInsumoPage.tsx';
 import { HomePage } from './feature/Home/HomePage.tsx';
 import { EquiposPage } from './feature/Equipos/pages/EquiposPage.tsx';
 import { EditarEquipoPage } from './feature/Equipos/pages/EditarEquipoPage.tsx';
 import { NuevoEquipoPage } from './feature/Equipos/pages/NuevoEquipoPage.tsx';
-import { PersonalPage } from './feature/Personal/PersonalPage.tsx';
-import { ListPage } from './feature/Insumos/pages/ListPage.tsx';
 
 const router = createBrowserRouter([
   {
@@ -15,14 +11,6 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <HomePage /> },
-      {
-        path: "insumos",
-        children: [
-          { index: true, element: <ListPage /> },             // /insumos
-          { path: "new", element: <NuevoInsumoPage /> },      // /insumos/new
-          { path: ":id/edit", element: <EditarInsumoPage /> },// /insumos/:id/edit
-        ],
-      },
       { path: "equipos", 
         children: [
           { index: true, element: <EquiposPage /> },
@@ -30,7 +18,6 @@ const router = createBrowserRouter([
           { path: ":id/edit", element: <EditarEquipoPage /> },
         ],
       },
-      { path: "personal", element: <PersonalPage /> },
     ]
   },
 ]);
