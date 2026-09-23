5b0426c7890a1e42eab16be3a8282f400acc3ae5
riquelme.juan261 <riquelme.juan261@gmail.com>
2026-09-23 04:36:02 -0300

    agregado recambio en init de src

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,3 +2,4 @@ from src.insumos.models import Insumo  # noqa: F401
 from src.personal.models import Persona  # noqa: F401
 from src.equipos.models import Equipo# noqa: F401
 from src.elementosLimpieza.models import ElementoLimpieza, TipoElementoLimpieza# noqa: F401
+from src.recambiosElementosLimpieza.models import RecambioElementoLimpieza
